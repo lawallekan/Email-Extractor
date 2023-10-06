c01b18498fce3e519306bc08756326b10d6a2796
Lawal Ridwan <lekanlawal83@gmail.com>
2023-10-06 12:35:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('extractBtn').addEventListener('click', function() {
+document.getElementById('extractBtn').addEventListener('click', function () {
     var inputText = document.getElementById('inputText').value;
     var extractedEmails = extractEmails(inputText);
     var validatedEmails = validateEmails(extractedEmails);
@@ -7,15 +7,15 @@ document.getElementById('extractBtn').addEventListener('click', function() {
     displayExtractedEmails(uniqueEmails);
 });
 
-document.getElementById('sortAlphabeticallyBtn').addEventListener('click', function() {
+document.getElementById('sortAlphabeticallyBtn').addEventListener('click', function () {
     sortEmails('alphabetically');
 });
 
-document.getElementById('sortByDomainBtn').addEventListener('click', function() {
+document.getElementById('sortByDomainBtn').addEventListener('click', function () {
     sortEmails('domain');
 });
 
-document.getElementById('sortByLengthBtn').addEventListener('click', function() {
+document.getElementById('sortByLengthBtn').addEventListener('click', function () {
     sortEmails('length');
 });
 
@@ -25,9 +25,9 @@ function applyFilters(emails) {
     var domainFilter = document.getElementById('domainFilter').value.toLowerCase();
     var keywordFilter = document.getElementById('keywordFilter').value.toLowerCase();
 
-    return emails.filter(function(email) {
+    return emails.filter(function (email) {
         return (domainFilter === '' || email.toLowerCase().includes(domainFilter)) &&
-               (keywordFilter === '' || email.toLowerCase().includes(keywordFilter));
+            (keywordFilter === '' || email.toLowerCase().includes(keywordFilter));
     });
 }
 
@@ -42,13 +42,13 @@ function sortEmails(sortBy) {
     if (sortBy === 'alphabetically') {
         emails.sort();
     } else if (sortBy === 'domain') {
-        emails.sort(function(a, b) {
+        emails.sort(function (a, b) {
             var domainA = a.split('@')[1].toLowerCase();
             var domainB = b.split('@')[1].toLowerCase();
             return domainA.localeCompare(domainB);
         });
     } else if (sortBy === 'length') {
-        emails.sort(function(a, b) {
+        emails.sort(function (a, b) {
             return a.length - b.length;
         });
     }
@@ -56,17 +56,17 @@ function sortEmails(sortBy) {
     outputText.value = emails.join('\n');
 }
 
-document.getElementById('extractBtn').addEventListener('click', function() {
+document.getElementById('extractBtn').addEventListener('click', function () {
     var inputText = document.getElementById('inputText').value;
     var extractedEmails = extractEmails(inputText);
     displayExtractedEmails(extractedEmails);
 });
 
-document.getElementById('csvBtn').addEventListener('click', function() {
+document.getElementById('csvBtn').addEventListener('click', function () {
     downloadEmails('csv');
 });
 
-document.getElementById('txtBtn').addEventListener('click', function() {
+document.getElementById('txtBtn').addEventListener('click', function () {
     downloadEmails('txt');
 });
 
@@ -85,19 +85,19 @@ function downloadEmails(format) {
     var outputText = document.getElementById('outputText').value;
     var filename = 'extracted_emails.' + format;
     var element = document.createElement('a');
-  
+
     if (format === 'csv') {
         outputText = 'Email\n' + outputText;
         element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(outputText));
     } else {
         element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(outputText));
     }
-  
+
     element.setAttribute('download', filename);
     element.style.display = 'none';
     document.body.appendChild(element);
-  
+
     element.click();
-  
+
     document.body.removeChild(element);
 }
